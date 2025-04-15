9dcf7847b2107380f1f94f3dd051f5f5c30e54d5
fevunge <developervunge@gmail.com>
2025-04-15 20:07:29 +0000

    NestFactory app now is a NestExpress app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
