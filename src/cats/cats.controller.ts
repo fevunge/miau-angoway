e37ed72d03119671cca93db79fcf441ac0ade4db
fevunge <developervunge@gmail.com>
2025-04-15 20:30:06 +0000

    NEW

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+    @Post()
+    create(): string {
+        return 'this action adds a new cat';
+    }
+
+    @Get()
+    findAll(): string {
+        return 'this action returns all cats';
+    }
+}
