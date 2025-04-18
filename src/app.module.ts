2db42a51e9fd6395af340ee9f1d07fa7aac16747
fevunge <developervunge@gmail.com>
2025-04-18 10:49:51 +0000

    App Module Updated with Insertion Of Users Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, PostsController],
+  controllers: [AppController, PostsController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
