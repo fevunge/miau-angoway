36679aab4cdf9b7dc51ef18d9fd739a75c9b83d0
fevunge <developervunge@gmail.com>
2025-04-18 11:51:32 +0000

    UsersService and PostsService included as providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { PostsService } from './posts/posts.service';
 import { PostsController } from './posts/posts.controller';
 import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [],
   controllers: [AppController, PostsController, UsersController],
-  providers: [AppService],
+  providers: [AppService, UsersService, PostsService],
 })
 export class AppModule {}
