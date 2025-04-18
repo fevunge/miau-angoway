d86f09edf94c803242654e0f8f49344e30156d51
fevunge <developervunge@gmail.com>
2025-04-18 11:52:18 +0000

    user/ Route created for signUpUser function

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from '@prisma/client';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor( private readonly userService: UsersService, )
+    @Post('user')
+    async signUpUser(
+        @Body() userData: {name? string; email; string},
+    ): Promise<User> {
+        return this.userService.createUser(userData);
+    }
+}
